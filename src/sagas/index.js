be26cb7aebb6dd3ec82563cffeb9d33eb2d52b89
Nav Garcha <nav.garcha@turner.com>
2016-07-22 19:14:22 +0100

    added todo comment

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,5 +2,6 @@ import { fork } from 'redux-saga/effects';
 import { watchFetchAlbums } from './albums';
 
 export default function* root() {
+	// @TODO: yield [watchFetchAlbums()];  Why not??
 	yield fork(watchFetchAlbums);
 }
