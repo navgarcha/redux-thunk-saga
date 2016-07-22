b98f92492dc2b9fc4e25b1534c5dac0ddf2fd592
Nav Garcha <nav.garcha@turner.com>
2016-07-22 12:58:06 +0100

    few control flow examples in redux-saga

--- src/sagas/albums.js
+++ src/sagas/albums.js
@@ -1,3 +1,4 @@
+import { delay } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
 import { fetch } from 'api';
 import { receiveAlbums } from 'actions/albums';
@@ -5,6 +6,7 @@ import { receiveAlbums } from 'actions/albums';
 export { ALBUMS_REQUEST } from 'actions/albums';
 
 export function* fetchAlbums() {
+	yield delay(1000);
 	const albums = yield call(fetch, '/albums');
 	yield put(receiveAlbums(albums));
 }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,8 @@
-import { takeLatest } from 'redux-saga';
+import { takeLatest, takeEvery } from 'redux-saga';
 import { fork } from 'redux-saga/effects';
 import { ALBUMS_REQUEST, fetchAlbums } from './albums';
 
 export default function* root() {
-	yield fork(takeLatest, ALBUMS_REQUEST, fetchAlbums);
+	// yield fork(takeLatest, ALBUMS_REQUEST, fetchAlbums);
+	yield fork(takeEvery, ALBUMS_REQUEST, fetchAlbums);
 }
