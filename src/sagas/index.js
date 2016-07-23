b01ebf5f4601e1bfb6a8302eb974dee2f59fefae
Nav Garcha <nav.garcha@turner.com>
2016-07-23 10:40:42 +0100

    parallel root saga tasks

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,6 +2,7 @@ import { fork } from 'redux-saga/effects';
 import { watchFetchAlbums } from './albums';
 
 export default function* root() {
-	// @TODO: yield [watchFetchAlbums()];  Why not??
-	yield fork(watchFetchAlbums);
+	yield [
+		fork(watchFetchAlbums)
+	];
 }
