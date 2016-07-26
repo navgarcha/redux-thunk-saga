b19c918ffc2305ca8cb0d75bb5969fc635450c62
Nav Garcha <nav.garcha@turner.com>
2016-07-27 00:16:01 +0100

    removed delay from api altogether

--- src/api.js
+++ src/api.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
 export function fetch(uri) {
-	return new Promise((resolve, reject) => {
-		axios.get(`https://jsonplaceholder.typicode.com${uri}`)
-            .then(({ data }) => resolve(data))
-			// .then(({ data }) => setTimeout(() => resolve(data), 1500))
-			.catch((error) => reject(error));
-	});
+	return axios.get(`https://jsonplaceholder.typicode.com${uri}`)
+        .then(({ data }) => data);
 }
