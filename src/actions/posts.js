1902f3aabe361ea6d56a254a97ffc8838d3a96d1
Nav Garcha <nav.garcha@turner.com>
2016-07-27 12:10:48 +0100

    code style tweak

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -3,18 +3,18 @@ import { request } from 'api';
 export const POSTS_RECIEVE = 'POSTS_RECIEVE';
 export const POSTS_CLEANUP = 'POSTS_CLEANUP';
 
+export function requestPosts() {
+    return (dispatch) => {
+        return request('/posts')
+            .then((posts) => dispatch(receivePosts(posts)))
+            .catch(() => console.log('Posts request failed!'));
+    };
+}
+
 function receivePosts(payload) {
 	return {
 		type: POSTS_RECIEVE,
 		payload
-	};
-}
-
-export function requestPosts() {
-	return (dispatch) => {
-		return request('/posts')
-            .then((posts) => dispatch(receivePosts(posts)))
-            .catch(() => console.log('Posts request failed!'));
 	};
 }
 
